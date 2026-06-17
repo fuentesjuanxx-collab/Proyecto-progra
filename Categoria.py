00deec981b442bdd93b1b23fef44ceae816a23d1
reniuxx <renataotarola2025@gmail.com>
2026-06-17 00:04:23 -0400

    Update Categoria.py

--- Categoria.py
+++ Categoria.py
@@ -1,3 +1,10 @@
 class Categoria:
     def __init__(self,nombre,id_categoria,descripcion):
-        pass
+        self.nombre = nombre
+        self.id_categoria = id_categoria
+        self.descripcion = descripcion
+    
+    def __str__ (self):
+        return f"{self.nombre}{self.id_categoria}{self.descripcion}"
+    
+    
